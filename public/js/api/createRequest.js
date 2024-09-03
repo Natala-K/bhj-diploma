3a4d81081d1bbc52ea287f6b81a0ef86c0910fac
Kolmy <test@neto.com>
2024-09-03 17:41:53 +0300

    added

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,53 +1,44 @@
 // createRequest.js
 
-function prepareRequestData(method, url, data) {
-    if (method === 'GET') {
-      const urlParams = new URLSearchParams(data).toString();
-      return url + '?' + urlParams;
-    } else {
-      const formData = new FormData();
-      for (const key in data) {
-        formData.append(key, data[key]);
-      }
-      return formData;
-    }
+function createRequestQuery(url, data) {
+  const urlParams = new URLSearchParams(data).toString();
+  return url + '?' + urlParams;
+}
+
+function createRequestForm(data) {
+  const formData = new FormData();
+  for (const key in data) {
+    formData.append(key, data[key]);
   }
-  
-  function sendRequest(method, url, data, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.responseType = 'json';
-  
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        callback(null, xhr.response);
-      } else {
-        callback(xhr.response, null);
-      }
-    };
-  
-    xhr.onerror = () => {
-      callback(xhr.response, null);
-    };
-  
-    if (method === 'GET') {
-      xhr.send();
-    } else {
-      xhr.send(data);
-    }
+  return formData;
+}
+
+function sendRequest(method, url, data, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.responseType = 'json';
+
+  xhr.onload = () => {
+    callback(null, xhr.response);
+  };
+
+  xhr.onerror = () => {
+    callback(xhr.response, null);
+  };
+
+  xhr.send(data);
+}
+
+function createRequest(options) {
+  const { method, url, data, callback } = options;
+  let requestQuery = url;
+  let requestForm = null;
+
+  if (method === 'GET') {
+    requestQuery = createRequestQuery(url, data);
+  } else {
+    requestForm = createRequestForm(data);
   }
-  
-  function createRequest(options) {
-    const { method, url, data, callback } = options;
-    let requestData;
-  
-    if (method === 'GET') {
-      requestData = prepareRequestData(method, url, data);
-      sendRequest(method, requestData, null, callback);
-    } else {
-      requestData = prepareRequestData(method, url, data);
-      sendRequest(method, url, requestData, callback);
-    }
-  }
-  
-  
+
+  sendRequest(method, requestQuery, requestForm, callback);
+}
