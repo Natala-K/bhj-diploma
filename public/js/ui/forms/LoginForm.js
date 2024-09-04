ad0c23bfbead1c13d9a44dd29e51852cea4bafc1
Kolmy <test@neto.com>
2024-09-04 23:23:04 +0300

    added authorization

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -6,10 +6,18 @@ class LoginForm extends AsyncForm {
   /**
    * Производит авторизацию с помощью User.login
    * После успешной авторизации, сбрасывает форму,
-   * устанавливает состояние App.setState( 'user-logged' ) и
+   * устанавливает состояние App.setState('user-logged') и
    * закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    User.login(data, (err, response) => {
+      if (response && response.success) {
+        App.setState('user-logged');
+        this.element.reset(); // Сброс формы
+        App.getModal('login').close(); // Закрытие модального окна
+      } else {
+        console.error(err || response.error);
+      }
+    });
   }
 }
